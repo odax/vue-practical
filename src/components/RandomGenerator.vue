40211ce842216d6423d1e8ee61048d26f863ff42
April Kragt <nr10@albion.edu>
2020-03-13 15:32:11 -0400

    validates input

--- src/components/RandomGenerator.vue
+++ src/components/RandomGenerator.vue
@@ -15,6 +15,9 @@
           {{result}}
         </li>
     </ul>
+    <div style="color: red;" v-if="error">
+      {{error}}
+    </div>
   </div>
 </template>
 
@@ -25,14 +28,53 @@ export default Vue.extend({
   data() {
     return {
       results: new Array<number>(),
+      error: null,
     };
   },
   methods: {
     clearResults: function () {
       this.results = [];
+      this.error = null;
     },
-   generate({ count = 1, min = 1, max = 10 } = { count: 1, min: 1, max: 10 }): void {
+    handleLargeCount(count) {
+      if (count > 10) {
+        this.error = "Count Can't be Greater than 10";
+        return null;
+      }
+    },
+    generate({ count = 1, min = 1, max = 10 } = { count: 1, min: 1, max: 10 }): void {
     this.clearResults();
+    //count check
+    if (count > 10) {
+        this.error = "Count Can't be Greater than 10";
+        return;
+      };
+    //check neg
+    if (Math.sign(count) === -1) {
+      this.error = "Count cannot be negative";
+      return;
+    }
+    if (Math.sign(min) === -1) {
+      this.error = "Min cannot be negative";
+      return;
+    }
+    if (Math.sign(max) === -1) {
+      this.error = "Max cannot be negative";
+      return;
+    }
+
+    //sane min/max
+    if (min > max) {
+      this.error = "Max must be greater than Min";
+      return;
+    }
+
+    //min/max above zero
+    if (min <= 0 || max <= 0) {
+      this.error = "Min and Max must both be above zero";
+      return;
+    }
+
     if (count > 1) {
     for (let i = 0; i <= count - 1; i++) {
       let ran = Math.floor(Math.random() * (max - min + 1) + min);
