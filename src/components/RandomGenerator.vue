37fa1c1bc5e7b0573d73905908d7a4822348cae6
April Kragt <nr10@albion.edu>
2020-03-13 16:28:32 -0400

    cleanup and fancy

--- src/components/RandomGenerator.vue
+++ src/components/RandomGenerator.vue
@@ -1,23 +1,35 @@
 <template>
-  <div>
-    MIN
-    <input id="min" v-model.number="min"/>
-    <br>
-    MAX
-    <input id="max" v-model.number="max"/>
-    <br>
-    COUNT
-    <input id="count" v-model.number="count"/>
-    <br>
-    <button id="btn" @click="generate({ count: count, min: min, max: max})">Generate</button>
-    <ul id="results" v-if="results">
-        <li v-for="result in results">
-          {{result}}
-        </li>
-    </ul>
-    <div style="color: red;" v-if="error">
-      {{error}}
+  <div class="container">
+    <div class="col-1">
+      <div class="col-1-item">
+      <span>Minimum</span> &nbsp;
+      <input id="min" v-model.number="min"/>
+      </div>
+      <div class="col-1-item">
+      <span>Maximum</span> &nbsp;
+      <input id="max" v-model.number="max"/>
+      </div>
+      <div class=" col-1-item">
+      <span>Count</span> &nbsp;
+      <input id="count" v-model.number="count"/>
+      </div>
     </div>
+    <div class="col-2">
+      <button id="btn" @click="generate({ count: count, min: min, max: max})">Generate</button>
+    </div>
+    <div class="col-3">
+      <div v-if="results && !error" class="list-container">
+      <ul id="results" class="list">
+          <li v-for="(result, index) in results" style="display: flex;">
+            {{result}}<div v-if="results.length > 1 && index != results.length - 1">,&nbsp;</div>
+          </li>
+      </ul>
+      </div>
+      <div v-if="error" class="error">
+        {{error}}
+      </div>
+    </div>
+    
   </div>
 </template>
 
@@ -29,68 +41,94 @@ export default Vue.extend({
     return {
       results: new Array<number>(),
       error: null,
+      min: null,
+      max: null,
+      count: null,
     };
   },
   methods: {
-    clearResults: function () {
+    clearResults() {
       this.results = [];
       this.error = null;
     },
     handleLargeCount(count) {
       if (count > 10) {
-        this.error = "Count Can't be Greater than 10";
+        this.error = 'Count Can\'t be Greater than 10';
         return null;
       }
     },
     generate({ count = 1, min = 1, max = 10 } = { count: 1, min: 1, max: 10 }): void {
-    this.clearResults();
-    //count check
-    if (count > 10) {
-        this.error = "Count Can't be Greater than 10";
+      this.clearResults();
+      //check count > 10
+      if (count > 10) {
+        this.error = 'Count Can\'t be Greater than 10';
         return;
-      };
-    //check neg
-    if (Math.sign(count) === -1) {
-      this.error = "Count cannot be negative";
+      }
+      // check neg
+      if (Math.sign(count) === -1) {
+      this.error = 'Count cannot be negative';
       return;
-    }
-    if (Math.sign(min) === -1) {
-      this.error = "Min cannot be negative";
-      return;
-    }
-    if (Math.sign(max) === -1) {
-      this.error = "Max cannot be negative";
-      return;
-    }
-
-    //sane min/max
-    if (min > max) {
-      this.error = "Max must be greater than Min";
-      return;
-    }
-
-    //min/max above zero
-    if (min <= 0 || max <= 0) {
-      this.error = "Min and Max must both be above zero";
-      return;
-    }
-
-    if (count > 1) {
-    for (let i = 0; i <= count - 1; i++) {
-      let ran = Math.floor(Math.random() * (max - min + 1) + min);
-      this.results.push(ran);
-    }
-    } else {
-      let ran = Math.floor(Math.random() * (max - min + 1) + min);
-
-      this.results.push(ran);
-    }
-    console.log(this.results);
-  }
+      }
+      if (Math.sign(min) === -1) {
+        this.error = 'Min cannot be negative';
+        return;
+      }
+      if (Math.sign(max) === -1) {
+        this.error = 'Max cannot be negative';
+        return;
+      }
+      // check sane min/max
+      if (min > max) {
+        this.error = 'Max must be greater than Min';
+        return;
+      }
+      //check min/max below zero
+      if (min <= 0 || max <= 0) {
+        this.error = 'Min and Max must both be above zero';
+        return;
+      }
+      //checks passed, generate nums for coun > 1
+      if (count > 1) {
+        for (let i = 0; i <= count - 1; i++) {
+          const ran = Math.floor(Math.random() * (max - min + 1) + min);
+          this.results.push(ran);
+        }
+        //generate nums for no count
+      } else {
+        const ran = Math.floor(Math.random() * (max - min + 1) + min);
+        this.results.push(ran);
+      }
+    },
   },
 });
 </script>
 
 <style scoped>
+  .container {
+    width: 80%;
+    margin: auto;
+    display: flex;
+  }
+  .col-1-item {
+    display: flex; justify-content: flex-end;
+  }
+  .col-1 {
+    display: flex; flex-direction: column; justify-content: space-between; height: 120px; width: 30%;
+  }
+  .col-2 {
+    width: 30%; display: flex; align-items: center; justify-content: center;
+  }
+  .col-3 {
+    width: 30%;
+  }
+  .list-container {
+    width: 100%; height: 100%; display: flex; align-items: center; justify-content: center;
+  }
+  .list {
+    list-style-type: none; display: flex; align-items: center; justify-content: center;
+  }
+  .error {
+    width: 100%; height: 100%; color: red; display: flex; justify-content: center; align-items: center;
+  }
 
 </style>
