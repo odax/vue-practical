50c91c48a9667029a925c24a24b58a4c1aaac01b
April Kragt <nr10@albion.edu>
2020-03-13 14:26:03 -0400

    random nums + input no typescript

--- src/components/RandomGenerator.vue
+++ src/components/RandomGenerator.vue
@@ -3,7 +3,7 @@
     <input id="min" v-model.number="min"/>
     <input id="max" v-model.number="max"/>
     <input id="count" v-model.number="count"/>
-    <button id="btn">Generate</button>
+    <button id="btn" @click="generate(6, 20, 30)">Generate</button>
     <ul id="results">
     </ul>
   </div>
@@ -19,9 +19,21 @@ export default Vue.extend({
     };
   },
   methods: {
-    generate({ count = 1, min = 1, max = 10 } = { count: 1, min: 1, max: 10 }): void {
+    // generate({ count = 1, min = 1, max = 10 } = { count: 1, min: 1, max: 10 }): void {
 
-    },
+  generate: function (count, min, max) {
+    if (count > 1) {
+    for (let i = 0; i <= count - 1; i++) {
+      let ran = Math.floor(Math.random() * (max - min + 1) + min);
+      this.results.push(ran);
+    }
+    } else {
+      let ran = Math.floor(Math.random() * (max - min + 1) + min);
+
+      this.results.push(ran);
+    }
+    console.log(this.results);
+  }
   },
 });
 </script>
